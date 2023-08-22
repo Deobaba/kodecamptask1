29c776a5035af963ec2063f25887530811bc800c
Adenekan Adeola <104663055+Deobaba@users.noreply.github.com>
2023-08-22 23:09:20 +0100

    shoppingCart

--- shopping cart.js
+++ shopping cart.js
@@ -15,8 +15,23 @@ function  Product (name,price) {
 }
 
 function ShoppingCart (){
+    this.item = [1,2]
+
+    this.getLength = function(){
+        return this.item.length 
+    }
+
+    this.addItem = function (product){
+        this.item.push(product)
+    }
+
+    this.removeItem
+
 
 }
 
 // let test = new product(1,2)
 // console.log(test.getName())
+let test = new ShoppingCart()
+test.addItem('product')
+console.log(test.getLength())
